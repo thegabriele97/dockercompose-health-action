500988cf1ec10a8c285ea981d552760265b63749
Alexey Kucherenko <alexei.kucherenko@gmail.com>
2024-04-19 09:42:11 +0300

    provide ability to pass custom docker-compose filename

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 
 cd ${INPUT_WORKDIR}
 
-docker-compose ps
-docker-compose logs
+docker-compose -f ${INPUT_FILENAME} ps
+docker-compose -f ${INPUT_FILENAME} logs
 
 regx='\s*([Rr]unning|[uU]p) \(healthy\)'
 secs=${INPUT_TIMEOUT}                           # Set interval (duration) in seconds.
@@ -14,16 +14,16 @@ while [ $(date +%s) -lt $endTime ]; do  # Loop until interval has elapsed.
         if [[ $line =~ $regx ]]; then
             cnt=$((cnt+1))
         fi
-    done <<< $(docker-compose ps)
-    echo -en "\rWaiting for services... $cnt/$(docker-compose ps | wc -l)"
-    if [[ $cnt -eq $(docker-compose ps | wc -l) ]]; then
+    done <<< $(docker-compose -f ${INPUT_FILENAME} ps)
+    echo -en "\rWaiting for services... $cnt/$(docker-compose -f ${INPUT_FILENAME} ps | wc -l)"
+    if [[ $cnt -eq $(docker-compose -f ${INPUT_FILENAME} ps | wc -l) ]]; then
         echo ""
         exit 0
     fi
     sleep 1
 done
 
-docker-compose ps
-docker-compose logs
+docker-compose -f ${INPUT_FILENAME} ps
+docker-compose -f ${INPUT_FILENAME} logs
 
 exit 1
